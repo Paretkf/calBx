1be0f7624008ca77160db0c6a6b433f4f586e5d6
Paretkf <paretk7@gmail.com>
2018-01-20 14:51:31 +0700

    edit home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,31 @@
     <section class="section">
       <div class="container">
         <div class="columns">
-          <div class="column is-4 is-offset-4">
-            {{user}}
-            <button class="button is-large"  @click="logout">logout </button>
+          <div class="column is-4 box">
           </div>
+
+          <div class="column is-6 box">
+
+          </div>
+
+          <div class="column box">
+            <br>
+            <div class="columns is-centered">
+              <img :src="user.fb.photoURL" alt="" class="circle">
+            </div>
+            <br>
+            <div class="columns is-centered">
+              <h1 class="title is-6">{{user.fb.displayName}}</h1>
+            </div>
+            <div class="columns is-centered">
+            </div>
+            <br>
+            <div class="columns is-centered">
+              <button @click="logout" class="button  is-danger is-outlined">logout</button>
+            </div>
+            <br>
+          </div>
+
         </div>
       </div>
     </section>
@@ -21,12 +42,33 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['logout'])
+    ...mapActions(['logout', 'getMargetData'])
   },
   computed: {
     ...mapGetters([
-      'user'
-    ])
+      'user',
+      'margetData'
+    ]),
+    img () {
+      return this.user.fb.photoURL
+    }
   }
 }
 </script>
+<style>
+  .circle{ /* ชื่อคลาสต้องตรงกับ <img class="circle"... */
+    height: auto;  /* ความสูงปรับให้เป็นออโต้ */
+    width: auto;  /* ความสูงปรับให้เป็นออโต้ */
+    border: 3px solid #fff; /* เส้นขอบขนาด 3px solid: เส้น #fff:โค้ดสีขาว */
+    border-radius: 50%; /* ปรับเป็น 50% คือความโค้งของเส้นขอบ*/
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2); /* เงาของรูป */
+  }
+  div{
+    font-family: 'Kanit', sans-serif;
+  }
+  .box{
+    margin-left: 2px;
+    margin-right: 2px;
+    margin-top: 2px;
+  }
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,21 @@
 
 export default {
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {}
+  state: {
+    margetData: {}
+  },
+  getters: {
+    margetData: state => state.margetData
+  },
+  mutations: {
+    GET_MARGET_DATA (state, data) {
+      state.margetData = data
+    }
+  },
+  actions: {
+    getMargetData ({commit}) {
+      fetch('//bx.in.th/api/').then((res) => res.json()).then((res) => {
+        commit('GET_MARGET_DATA', res)
+      })
+    }
+  }
 }
